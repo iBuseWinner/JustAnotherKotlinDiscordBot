ced18f53246ff6235a1db18a7cec71ffe93efc48
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2020-09-20 18:33:02 +0300

    Add util for adding reasons (warns again)

--- src/main/kotlin/jakdb/utils/Calculat.kt
+++ src/main/kotlin/jakdb/utils/Calculat.kt
@@ -1,6 +1,8 @@
 package jakdb.utils
 
+import jakdb.data.mysql.getWarnReasonsForUser
 import jakdb.data.mysql.getWarnsPunish
+import jakdb.data.mysql.setWarnReasonsForUser
 import jakdb.data.mysql.setWarnsPunish
 
 @Synchronized
@@ -33,4 +35,36 @@ fun getAllWarnsAndRemoveOld(guild: Long, count: Int) {
     setWarnsPunish(guild, sql)
 }
 
+/**
+ * It adds reason after last reason.
+ */
+@Synchronized
+fun getAllUserWarnsAndAddNew(guild: Long, user: Long, reason: String) {
+    val warns = getWarnReasonsForUser(guild, user)
+    setWarnReasonsForUser(guild, user, "$warns,{\"$reason\"}")
+}
 
+/**
+ * It removes only last reason.
+ * It will not remove specific reason, because it can't do this.
+ */
+@Synchronized
+fun getAllUserWarnsAndRemoveOld(guild: Long, user: Long) {
+    val warns = getWarnReasonsForUser(guild, user)
+    val w = warns.split(",")
+
+    var i = 0
+    val ww = HashMap<String, String>()
+    for(d in w) {
+        ww["$i"] = d
+        i++
+    }
+
+    ww.remove("$i")
+
+    var total = ""
+    for(d in ww.keys) {
+        total = "$total,{\"${ww[d]}\"}"
+    }
+    setWarnReasonsForUser(guild, user, total)
+}
